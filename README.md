234b8f838ca4de113aac09caf5428fed59f99d04
Ceci007 <cecibot9@gmail.com>
2022-05-01 15:54:38 -0400

    finished game

--- README.md
+++ README.md
@@ -1,70 +1,36 @@
-# Getting Started with Create React App
+#  🧐 2048 Game
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+> This is a Game made with react that resembles the 2048 Game and rules, also it has some cool animations.
 
-## Available Scripts
+![screenshot](./app_screenshot.jpg) 
 
-In the project directory, you can run:
+## 🔧 Built With
 
-### `npm start`
+- HTML
+- Sass (.scss extension)
+- Using flexbox and CSS grid
+- Using mediaqueris for Responsive Design
+- .gif files
+- React 
+- Class based logic (javascript)
+- Hooks 
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## 🔴 Live Demo
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+[Live Demo]()
 
-### `npm test`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## 🛠 Getting Started
 
-### `npm run build`
+To get a local copy up and running follow these simple example steps.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+- Go to the main page of te repo.
+- Press the ```Code``` button and get the repo link.
+- Clone it using git.
+- run ```npm install``` command.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+## ✒️ Authors
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+👤 **Cecilia Benitez**
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- [@Ceci007](https://github.com/Ceci007)
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,41 @@
 import React, { useState } from 'react';
+import Modal from 'react-modal';
 import Tile from './Tile';
 import Cell from './Cell';
 import GameOverlay from './GameOverlay';
 import useEvent from '../hooks/useEvent';
 import { Board } from '../helper';
 
+const customStyles = {
+    content: {
+        position: 'absolute',
+        top: '40px',
+        left: '40px',
+        right: '40px',
+        bottom: '40px',
+        background: '#57407c',
+        overflow: 'auto',
+        WebkitOverflowScrolling: 'touch',
+        borderRadius: '9px',
+        outline: 'none',
+        padding: '20px'
+      }
+};
+
+Modal.setAppElement('#root');
+
 export default function BoardView() {
     const [board, setBoard] = useState(new Board());
+    const [modalIsOpen, setIsOpen] = React.useState(false);
     const year = new Date().getFullYear(); 
+
+    const openModal = () => {
+        setIsOpen(true);
+    }
+
+    const closeModal = () => {
+        setIsOpen(false);
+    }
 
     const cells = board.cells.map((row, rowIndex) => {
         return (
@@ -44,18 +72,15 @@ export default function BoardView() {
 
     return (
         <div>
-            <div className='details-container'>
+            <div className='fixed-container'>
                 <div className='details-box'>
-                    <div className='resetButton' onClick={resetGame}>New Game</div>
+                    <div className='buttons'>
+                        <div className='resetButton margin-right' onClick={resetGame}>New Game</div>
+                        <div className='resetButton' onClick={openModal}>Game Rules</div>
+                    </div>
                     <div className='score-box'>
                         <div className='score-header'>SCORE</div>
                         <div>{board.score}</div>
-                    </div>
-                </div>
-                <div className='details-box'>
-                    <div className='text'>
-                        <span>Play 2048 Game Online</span>
-                        <p>Join the numbers and get to the <span>2048 tile!</span></p>
                     </div>
                 </div>
             
@@ -64,24 +89,38 @@ export default function BoardView() {
                     <GameOverlay onRestart={resetGame} board={board} />
                 </div>               
             </div>
-
-           <footer>
-               <div className='footer-rules'>
-                <div className='details-box'>
-                        <div className='text'>
-                            <span>How to Play:</span>
-                            <p>
-                            Use your <span>arrow keys</span> to move the tiles. When two tiles with the same number touch, they <span>merge into one!</span>
-                            </p>
-                        </div>
+            { modalIsOpen && (
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                >
+                    <div>
+                        <div className='footer-rules'>
+                                <div className='details-box'>
+                                    <div className='text'>
+                                        <span>Play 2048 Game Online</span>
+                                        <p>Join the numbers and get to the <span>2048 tile!</span></p>
+                                    </div>
+                                </div>
+                                <div className='details-box'>
+                                    <div className='text'>
+                                        <span>How to Play:</span>
+                                        <p>
+                                        Use your <span>arrow keys</span> to move the tiles. When two tiles with the same number touch, they <span>merge into one!</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='copyright'>
+                                <p>
+                                <small>&copy; Copyright {year}</small>, Cecilia Benitez
+                                </p>
+                                <div className='resetButton' onClick={closeModal}>Cancel</div>
+                            </div>
                     </div>
-                </div>
-                <div className='copyright'>
-                    <p>
-                    <small>&copy; Copyright {year}</small>, Cecilia Benitez
-                    </p>
-                </div>
-           </footer>
+                </Modal>
+            )}
         </div>
     )
 }
